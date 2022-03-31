f5c1ae9313633b94b9eb1c35ba21746f52361149
HAKAN KAYA <hakankaya92@gmail.com>
2022-03-31 17:06:59 +0300

    js odev 2 todoList_latest

--- js_odev_2_toDoList/js/todoList.js
+++ js_odev_2_toDoList/js/todoList.js
@@ -1,31 +1,45 @@
-// onLoad();
-// listemize ekleme yapiyoruz.
+onLoad();
+// listemize ekleme yaptigimiz fonksiyon
 function newElement(){
     let taskDOM = document.querySelector("#task");
     let inputLi = taskDOM.value; 
+    taskDOM.value = ""; //input bi degere atanıp documan ici bosaltılır.
     let control = Array.from(inputLi).filter(deger => deger != " ");
-    console.log(control + "uzunluk "+control.length);
     
     if(control.length < 1 ){
         // alert("boş todolist yapamazsınız tekrar giriniz");
         $('.error').toast("show");
     }else{
-        taskDOM.value = ""; //input bi degere atanıp documan ici bosaltılır.
-        // console.log(inputLi);
         let myLi = document.createElement('li');
         myLi.textContent = inputLi;
+        myLi.innerHTML += `<button style="padding-right: 5px; padding-top:5px;" class="close">&times</button>`; 
         // console.log(myLi);
         let liDOM = document.querySelector("#list");
         liDOM.appendChild(myLi);
         var toStorage = liDOM.innerHTML;
-        // console.log(toStorage);
         localStorage.setItem('listemiz', JSON.stringify(toStorage));
         $( '.success').toast("show");
     }
 }
+function delElement(){
 
+}
 // sayfa başta yüklenirken mevcut todolist i yukleyecek
+
 function onLoad(){
+    let listDOM = document.querySelector("#list");
+    listDOM.innerHTML = JSON.parse(localStorage.getItem('listemiz'));
+    listDOM.addEventListener('click', clickEvent);
+}
+
+function clickEvent(e){
+    if (e.target.className == 'close') {
+        e.target.parentElement.remove();
+    }else{
+        e.target.classList.toggle('checked');
+
+    }
     let liDOM = document.querySelector("#list");
-    liDOM.innerHTML = JSON.parse(localStorage.getItem('listemiz'));
+    var toStorage = liDOM.innerHTML;
+    localStorage.setItem('listemiz', JSON.stringify(toStorage));
 }
